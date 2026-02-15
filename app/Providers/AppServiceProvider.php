b2b619010847efd0c6732239d51b61fc5f396aae
Andrew <aandrewjuan1@gmail.com>
2026-02-15 17:37:29 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use WorkOS\WorkOS as WorkOSSDK;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        $this->configureWorkOS();
+    }
+
+    protected function configureWorkOS(): void
+    {
+        $apiKey = config('services.workos.secret');
+        $clientId = config('services.workos.client_id');
+
+        if ($apiKey) {
+            WorkOSSDK::setApiKey($apiKey);
+        }
+        if ($clientId) {
+            WorkOSSDK::setClientId($clientId);
+        }
     }
 
     protected function configureDefaults(): void
